e304278369eb4b240f5e510acd372bff80bd1756
Benoit SCHULER <benoit.schuler@wisper.io>
2023-05-11 16:05:04 +0200

    documentation god

--- distance.py
+++ distance.py
@@ -24,8 +24,14 @@ critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
 
 
 def euclid_on_maps_normalized(map_a, map_b):
-    """
-    Returns euclidian distance between maps map_a and map_b
+    """computes euclidian distance normalized between the maps of two persons
+
+    Args:
+        map_a (hashmap movie:notes): the score given by a to each movie
+        map_b (hashmap movie:notes): the score given by b to each movie
+
+    Returns:
+        float: the euclidian distance normalized, float between 0 and 1
     """
     euclid_dist = 0
     for key in map_b.keys():
@@ -35,8 +41,14 @@ def euclid_on_maps_normalized(map_a, map_b):
 
 
 def get_friends_map(person, distance_function):
-    """
-    create a map with all other persons as key and person's distance with them as value
+    """get friends map 
+
+    Args:
+        person (string): the person you want to know similarities with
+        distance_function (function hashmap:hashmap): the function you use to compute the distance between the maps of two persons
+
+    Returns:
+        hashmap person:float : distance with each other persons in the critics
     """
     friends = {}
     for key in critics.keys():
@@ -45,12 +57,15 @@ def get_friends_map(person, distance_function):
                 critics[person], critics[key])})
     return friends
 
-# print(friends_map('Lisa Rose',euclid_on_maps_normalized))
+# print(get_friends_map('Lisa Rose',euclid_on_maps_normalized))
 
 
 def rank_friends(person, distance_function):
-    """
-    rank friends from closest to farthest using the chosen function
+    """pretty print ranking with the other people in the critics order by their similarities with person
+
+    Args:
+        person (string): the person you want to know similarities with
+        distance_function (function hashmap:hashmap): the function you use to compute the distance between the maps of two persons
     """
     friends_map = get_friends_map(person, distance_function)
     print('--------------------')
@@ -61,8 +76,14 @@ def rank_friends(person, distance_function):
 
 
 def pearson_on_maps(map_a, map_b):
-    """
-    pearson distance with maps
+    """computes pearson distance between the maps of two persons
+
+    Args:
+        map_a (hashmap movie:notes): the score given by a to each movie
+        map_b (hashmap movie:notes): the score given by b to each movie
+
+    Returns:
+        float: the pearson distance, float between -1 and 1
     """
     common_films = sum_a = sum_b = sum_axb = sum_square_a = sum_square_b = 0
     for key in map_b.keys():
@@ -90,6 +111,8 @@ rank_friends('Toby', pearson_on_maps)
 
 
 def get_all_films():
+    """retrieves all films from the critics
+    """
     all_films = []
     for person in critics.values():
         for film in person.keys():
@@ -101,6 +124,11 @@ def get_all_films():
 
 
 def get_all_film_unwatched_by(person):
+    """retrieves all films the person has not watched
+
+    Args:
+        person (string): a key of the critics
+    """
     films = get_all_films()
     for film in critics[person].keys():
         films.remove(film)
@@ -110,6 +138,15 @@ def get_all_film_unwatched_by(person):
 
 
 def get_recommandation(friend_map, movie):
+    """recommends a movie to someone
+
+    Args:
+        friend_map (hashmap person : similarity): friend_map of a given person
+        movie (string): a movie that person has not watched
+
+    Returns:
+        float: recommendation
+    """
     sim_sum = sx_sum = 0
     for person in friend_map.keys():
         if critics[person].keys().__contains__(movie) and friend_map[person] >= 0:
@@ -122,6 +159,15 @@ def get_recommandation(friend_map, movie):
 
 
 def get_recommandation_map(person, distance_function):
+    """generates a map made of the unwatched movies and recommandations of somebody
+
+    Args:
+        person (string): the person you want to recommend a movie to
+        distance_function (function hashmap:hashmap): the function you use to compute the distance between the maps of two persons
+
+    Returns:
+        hashmap movie:recommandation_score
+    """
     unwatched_films = get_all_film_unwatched_by(person)
     friend_map = get_friends_map(person, distance_function)
     recommandation_map = {}
@@ -130,7 +176,13 @@ def get_recommandation_map(person, distance_function):
     return recommandation_map
 
 
-def rank_movie(person, distance_function):
+def rank_movies(person, distance_function):
+    """pretty prints the recommandation map 
+
+    Args:
+        person (string): the person you want to recommend a movie to
+        distance_function (function hashmap:hashmap): the function you use to compute the distance between the maps of two persons
+    """
     recommandation_map = get_recommandation_map(person, distance_function)
     print('--------------------')
     print(
@@ -141,4 +193,7 @@ def rank_movie(person, distance_function):
     print('--------------------')
 
 
-rank_movie('Toby', pearson_on_maps)
+rank_movies('Toby', pearson_on_maps)
+
+rank_friends('Toby', euclid_on_maps_normalized)
+rank_movies('Toby', euclid_on_maps_normalized)
